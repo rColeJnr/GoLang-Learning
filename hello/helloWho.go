c07f747750d34e94079f49f88e4a869034f789dd
rcolejnr <ricardo.m.jnr@gmail.com>
2023-03-07 22:24:28 +0300

    Hello who?

--- hello/helloWho.go
+++ hello/helloWho.go
@@ -3,17 +3,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
+	"fmt" // provides funcst for text formatting and reading fortmatted text
+	"os" //provides platform-independent os variables and fncs
+	"strings" // provides funcs for manipulatins strs
 )
 
 func main() {
-	who := "rCole!"
+	who := "rCole!" // short var declaration. declares and inits a variable at the same time
 
 	if len(os.Args) > 1 {
 		/* so.Args[0] is "hello" or "hello.exe" */
-		who = strings.Join(os.Args[1:], " ")
+		who = strings.Join(os.Args[1:], " ") // assignment op
 	}
 
 	fmt.Println("Hello", who)
