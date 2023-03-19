a3571d356100c7e0bb6568200227cad2eafde01e
rColeJnr <ricardo.m.jnr@gmail.com>
2023-03-20 00:38:24 +0300

    Shortening and unshort urls with base62 and postgres

--- BuildingRestfulApis/postgrees/main.go
+++ BuildingRestfulApis/postgrees/main.go
@@ -1,16 +1,87 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"rcole/postgres/base62"
 	"rcole/postgres/models"
+	"time"
 )
+
+// DB stores the database session information, Need t be intialized once
+type DBClient struct {
+	db *sql.DB
+}
+
+// Model the record struct
+type Record struct {
+	ID  int    `json:"id"`
+	URL string `json:"url"`
+}
+
+// GetOriginal URL fetches the original URL for the given encoded string
+func (driver *DBClient) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
+	var url string
+	vars := mux.Vars(r)
+	// Get id from base62 string
+	id := base62.ToBase10(vars["encoded_string"])
+	err := driver.db.QueryRow("SELECT url FROM web_url WHERE id = $1", id).Scan(&url)
+	// Handle response details
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		responseMap := map[string]interface{}{"url": url}
+		response, _ := json.Marshal(responseMap)
+		w.Write(response)
+	}
+}
+
+// GenerateShortURL adds URL to DB  and gives back shortened string
+func (driver *DBClient) GenerateShortURL(w http.ResponseWriter, r *http.Request) {
+	var id int
+	var record Record
+	postBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(postBody, &record)
+	err := driver.db.QueryRow("INSERT INTO web_url(url) VALUES($1) RETURNING id", record.URL).Scan(&id)
+	responseMap := map[string]interface{}{"encoded_string": base62.ToBase62(id)}
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(responseMap)
+		w.Write(response)
+	}
+}
 
 func main() {
 	db, err := models.InitDB()
 
 	if err != nil {
-		log.Println(db, "this") // printing the db connection, which will be an address.
+		panic(err) // printing the db connection, which will be an address.
 	}
-	log.Println(err)
+	dbclient := &DBClient{db: db}
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	// Create a new router
+	r := mux.NewRouter()
+	// attach an elegant path with handler
+	r.HandleFunc("/v1/short/{encoded_string:[a-zA-Z0-9]*}", dbclient.GetOriginalURL).Methods("GET")
+	r.HandleFunc("/v1/short", dbclient.GenerateShortURL).Methods("POST")
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":1205",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
