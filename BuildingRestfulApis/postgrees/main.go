8ead1315dabfaf2bcd417c65c7f622d3711c85c2
= <=>
2023-03-19 14:48:08 +0300

    PostgreSQL basics

---
+++ BuildingRestfulApis/postgrees/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"rcole/postgres/models"
+)
+
+func main() {
+	db, err := models.InitDB()
+	if err != nil {
+		log.Println(db, "this") // printing the db connection, which will be an address.
+	}
+	log.Println(err)
+
+}
