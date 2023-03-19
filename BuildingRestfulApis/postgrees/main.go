6fc1b19d54a532e4c17547d4e115e30e2447bacc
= <=>
2023-03-19 15:02:55 +0300

    why hub no work ok?

--- BuildingRestfulApis/postgrees/main.go
+++ BuildingRestfulApis/postgrees/main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
+	name("Rick")
 	db, err := models.InitDB()
+
 	if err != nil {
 		log.Println(db, "this") // printing the db connection, which will be an address.
 	}
 	log.Println(err)
 
+}
+
+func name(s string) {
+	log.Printf("dudes name %v\n", s)
 }
