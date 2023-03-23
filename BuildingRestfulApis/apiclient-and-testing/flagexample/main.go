6f32a0c6b0749b11be881a1d93cd5f7a6253fa3f
rColeJnr <ricardo.m.jnr@gmail.com>
2023-03-23 22:07:24 +0300

    Working with flag = eazy

--- BuildingRestfulApis/apiclient-and-testing/flagexample/main.go
+++ BuildingRestfulApis/apiclient-and-testing/flagexample/main.go
@@ -10,8 +10,14 @@ import (
 
 var name = flag.String("name", "stranger", "your wonderful name")
 var age = flag.Int("age", 1, "your wonderful age")
+var girlfriend string
+
+func init() {
+	flag.StringVar(&girlfriend, "girlfriend", "Jessica", "Your dream girl, bcs you are single")
+}
 
 func main() {
 	flag.Parse()
 	fmt.Printf("Hello sir %s who has wondered this lands for %d weeks, and certainly more to come, Welcome to the command line world\n", *name, *age)
+	fmt.Printf("Keep going, it will all workout one day, you will find the girl and you'll live the live you dream for you and your family and %s\n", girlfriend)
 }
